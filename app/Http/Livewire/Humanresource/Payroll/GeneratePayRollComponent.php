7dc13e0596060349460c9059a27bc9b88b981260
TimothyMuyingo <muytimothy@gmail.com>
2023-06-12 18:58:22 +0300

    Payslips notification implementation

--- app/Http/Livewire/Humanresource/Payroll/GeneratePayRollComponent.php
+++ app/Http/Livewire/Humanresource/Payroll/GeneratePayRollComponent.php
@@ -6,6 +6,7 @@ use Carbon\Carbon;
 use Livewire\Component;
 use App\Models\Department;
 use App\Models\Humanresource\Employee;
+use App\Jobs\HumanResource\SendPaySlip;
 use App\Models\Settings\GeneralSetting;
 
 class GeneratePayRollComponent extends Component
@@ -14,6 +15,9 @@ class GeneratePayRollComponent extends Component
     public $from_date, $to_date, $department_id, $employee_id;
     public $usd_rate, $show_month, $global=null;
     public $emp_payroll = [];
+    public $employeeIds = [];
+    public $selectedEmployeeIds=[];
+
     public function mount()
     {
         if($this->show_month == null){
@@ -31,6 +35,38 @@ class GeneratePayRollComponent extends Component
         ->when($this->employee_id, function ($query) {$query->where('id', $this->employee_id);})
         ->when($this->department_id, function ($query) {$query->where('department_id', $this->department_id);})
         ->get();
+
+        $this->employeeIds=$this->emp_payroll->pluck('id')->toArray();
+    }
+
+    public function sendPayslip()
+    {
+        if(count($this->selectedEmployeeIds)>0){
+
+            try {
+                SendPaySlip::dispatch($this->selectedEmployeeIds);
+                // $this->dispatchBrowserEvent('alert', ['type' => 'success',  'message' => 'Payslip sent successfully']);
+                $this->dispatchBrowserEvent('swal:modal', [
+                    'type' => 'success',
+                    'message' => 'Payslip sent',
+                    'text' => 'Payslip have/has been sent successfully',
+                ]);
+
+            } catch (\Throwable $th) {
+                $this->dispatchBrowserEvent('swal:modal', [
+                    'type' => 'error',
+                    'message' => 'Oops! Sending failed!',
+                    'text' => 'Something went wrong and Payslip could not be sent. Please try again.',
+                ]);
+            }
+
+        }else{
+            $this->dispatchBrowserEvent('swal:modal', [
+                'type' => 'warning',
+                'message' => 'Oops! Not Found!',
+                'text' => 'No Employees selected for this operation!',
+            ]);
+        }
     }
 
     public function render()
---
+++ app/Mail/HumanResource/PayslipEmail.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\Mail\HumanResource;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Notifications\Notification;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+
+class PayslipEmail extends Notification
+{
+    use Queueable;
+
+    private $details;
+
+    /**
+     * Create a new notification instance.
+     *
+     * @return void
+     */
+    public function __construct($details)
+    {
+        $this->details = $details;
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function via($notifiable)
+    {
+        return ['mail'];
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return \Illuminate\Notifications\Messages\MailMessage
+     */
+    public function toMail($notifiable)
+    {
+        return (new MailMessage)
+                    ->subject('Your Payslip')
+                    ->greeting($this->details['greeting'])
+                    ->line($this->details['body'])
+                    ->action($this->details['actiontext'], $this->details['actionurl'])
+                    ->line('Thank you for using our application!')
+                    ->attach($this->details['payslip_path'], [
+                        'as' => 'payslip.pdf',
+                        'mime' => 'application/pdf',
+                    ]);
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function toArray($notifiable)
+    {
+        return [
+            //
+        ];
+    }
+}
