cb9109a3c76fecbcd9b980189a94b9c495ab8f6e
ked-k <kedkayz@gmail.com>
2023-07-10 15:18:45 +0300

    Update EmployeeController.php

--- app/Http/Controllers/Humanresource/EmployeeController.php
+++ app/Http/Controllers/Humanresource/EmployeeController.php
@@ -488,29 +488,33 @@ class EmployeeController extends Controller
     public function downloadProjectPayslip($contract_id,$currency,$month,$prepared_by)
     {
         $emp_id = null;
+        $print_month= Carbon::parse($month)->format('F-Y');
+        $month_date = Carbon::parse($month)->format('Y-m-d');
         $data['month_value'] = $month;
         $data['month'] = Carbon::today()->format('Y-m-d');
         $data['currency'] =$currency;
         $data['global'] = GeneralSetting::latest()->first();        
         // $data['approvaler']=Employee::where('id', $approver_id)->first();
         $data['prepper']=$prepared_by;
-        $data['employee'] =$employee= ProjectContract::with('employee','project','position')->where('id', $contract_id)->first();
+        $data['employee'] =$employee= ProjectContract::with('employee','project','position')->where('id', $contract_id)->where('start_date','<=', $month_date)->where('end_date','>=', $month_date)->first();
         if($employee){
             $emp_id=$employee->employee_id;
-        }
-        $data['bank_account']= $bank_account = BankingInformation::where(['employee_id'=> $emp_id, 'is_default'=>1])->latest()->first();
-        if(!$bank_account){            
-            $data['bank_account'] = BankingInformation::where('employee_id', $emp_id)->latest()->first();
-        }
-        // return View('downloads.project-pay-slip', $data);
-        $pdf = PDF::loadView('downloads.project-pay-slip',$data);
-        $pdf->setPaper('a4', 'portrait');   //horizontal
-        $pdf->getDOMPdf()->set_option('isPhpEnabled', true);
+            $data['bank_account']= $bank_account = BankingInformation::where(['employee_id'=> $emp_id, 'is_default'=>1])->latest()->first();
+            if(!$bank_account){            
+                $data['bank_account'] = BankingInformation::where('employee_id', $emp_id)->latest()->first();
+            }
+            // return View('downloads.project-pay-slip', $data);
+            $pdf = PDF::loadView('downloads.project-pay-slip',$data);
+            $pdf->setPaper('a4', 'portrait');   //horizontal
+            $pdf->getDOMPdf()->set_option('isPhpEnabled', true);
 
-        return  $pdf->stream($employee->employee?->fullname.'_'.$month.' Payslip.pdf');
+            return  $pdf->stream($employee->employee?->fullname.' '.$print_month.' Payslip.pdf');
 
-
-        // return $pdf->download($testResult->sample->participant->identity.rand().'.pdf');
+         }else{
+            return 'Sorry no employee contract record between that particular time period selected';
+         }
+        // hey u r ur  ru hrk ir irjj vn   ej  
+        
     }
 
     /**
