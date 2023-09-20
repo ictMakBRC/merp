76cf1644de8d8bceb40fc150a176cb77cb585ae0
ked-k <kedkayz@gmail.com>
2023-09-20 16:02:47 +0300

    Update 2023_08_12_155853_create_activity_logs_table.php

--- database/migrations/2023_08_12_155853_create_activity_logs_table.php
+++ database/migrations/2023_08_12_155853_create_activity_logs_table.php
@@ -13,16 +13,16 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('activity_logs', function (Blueprint $table) {
-            $table->bigIncrements('id');
-            $table->unsignedBigInteger('user_id')->nullable()->index('activity_logs_user_id_foreign');
-            $table->string('email')->nullable();
-            $table->string('description')->nullable();
-            $table->string('platform')->nullable();
-            $table->string('browser')->nullable();
-            $table->string('client_ip', 45)->nullable();
-            $table->timestamps();
-        });
+        // Schema::create('activity_logs', function (Blueprint $table) {
+        //     $table->bigIncrements('id');
+        //     $table->unsignedBigInteger('user_id')->nullable()->index('activity_logs_user_id_foreign');
+        //     $table->string('email')->nullable();
+        //     $table->string('description')->nullable();
+        //     $table->string('platform')->nullable();
+        //     $table->string('browser')->nullable();
+        //     $table->string('client_ip', 45)->nullable();
+        //     $table->timestamps();
+        // });
     }
 
     /**
